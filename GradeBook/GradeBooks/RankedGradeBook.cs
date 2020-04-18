65b9d6a3a4bbbbbc3ce95b3366db6c088eefac35
Chris Holleworth <cholleworth@ematics.co.uk>
2020-04-18 11:26:28 +0100

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,30 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count >= 5)
+            {
+                switch (averageGrade)
+                {
+                    case var g when g >= 80:
+                        return 'A';
+                    case var g when g >= 60:
+                        return 'B';
+                    case var g when g >= 40:
+                        return 'C';
+                    case var g when g >= 20:
+                        return 'D';
+                    default:
+                        return 'F';
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+
+        }
     }
 }
